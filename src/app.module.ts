4b3dc88b643d373008970cf77f1618fc9c19ce06
Aleksandr <a.teslin90@gmail.com>
2023-08-18 17:10:38 +0300

    feat: change controller to run db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { ArtistsModule } from './artists/artists.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'src/datasource';
+import { IamModule } from './iam/iam.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -17,6 +19,8 @@ import { dataSourceOptions } from 'src/datasource';
     ArtistsModule,
     FavoritesModule,
     TypeOrmModule.forRoot(dataSourceOptions),
+    IamModule,
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
